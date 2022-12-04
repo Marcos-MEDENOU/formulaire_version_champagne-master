87b4ab0353501f734714b1f198d934df0bde1b1b
Marcos MEDENOU <marcosmedenou@gmail.com>
2022-12-04 13:55:45 +0100

    email part

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -14,22 +14,46 @@ class RegisterModel extends Connection {
         $this->password = $password;
     }
 
+        public function verify_email_without_inscription(){
+              
+            try {
+                $conn = $this->connect();
+                $sql = "SELECT * FROM userss WHERE useremail=:mail";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([':mail' => $this->email,]);
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    header("location: ../views/Sig_up.php?msg=inavlid"); 
+                } else {
+                    return 'false';
+                }
+               // $stmt->closeCursor();//Interrompre la recherche 
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
         public function registerUser() {
         // Connection à la BDD
         try {
             //code...
             $conn = $this->connect();
-            $sql = "INSERT INTO 
+            if($this->verify_email_without_inscription()=='false'){
+                $sql = "INSERT INTO 
             `db`.userss VALUES ( NULL,
             :userlastname, :userfirstname, :username, :useremail, :userpassword)";
-            $stmt = $conn->prepare($sql);
-            $stmt = $stmt->execute([
-                ":userlastname" => $this->lastname,
-                ":userfirstname" => $this->firstname,
-                ":username" => $this->username,
-                ":useremail" => $this->email,
-                ":userpassword" => password_hash($this->password, PASSWORD_DEFAULT),
-            ]);
+                $stmt = $conn->prepare($sql);
+                $stmt = $stmt->execute([
+                    ":userlastname" => $this->lastname,
+                    ":userfirstname" => $this->firstname,
+                    ":username" => $this->username,
+                    ":useremail" => $this->email,
+                    ":userpassword" => password_hash($this->password, PASSWORD_DEFAULT),
+                ]);
+            }else {
+                header("location: ../views/Sig_up.php?msg=inavlid"); 
+            }
+            
         } catch (PDOException $e) {
             //throw $th;
            echo $e->getMessage();        
@@ -37,31 +61,3 @@ class RegisterModel extends Connection {
     }
 }
 
-
-
-    // public function emailuserDB()
-    // {
-    //     try {
-    //         $conn = $this->connect();
-    //         $sql = "SELECT * FROM userss WHERE useremail=:mail";
-    //         $stmt = $conn->prepare($sql);
-    //         // $stmt1 = $conn->prepare($sql1);
-    //         $stmt->execute([':mail' => $this->emailuser,]);
-    //         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    //         if ($user && password_verify($this->passworduser, $user['userpassword'])) {
-    //             $userfirstname= $user['userfirstname'];
-    //              $userlastname= $user['userlastname'];
-    //              $username= $user['username'];
-    //              $password=$user['userpassword'];
-    //             header("location:../views/Dashboard.php?msg=userinformations&userfirstname=$userfirstname&userlastname=$userlastname&username=$username&passcrypt=$password&end");
-
-    //         } else {
-    //            // header("location:../views/Dashboard.php?msg=userinformations&userfirstname");
-    //             header("location: ../views/Sig_in.php?msg=inavlid&usermail=$this->emailuser"); 
-
-    //         }
-    //         $stmt->closeCursor();//Interrompre la recherche 
-    //     } catch (PDOException $e) {
-    //         echo $e->getMessage();
-    //     }
-    // }
